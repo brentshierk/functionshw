ee1766b902e7d4116cb010830a7ace45bbceee98
Brent_Shierk <bshierk1996@gmail.com>
2020-09-08 22:24:49 -0400

    compleated hw

---
+++ functions.py
@@ -0,0 +1,30 @@
+#questions 1-3
+def area(width,height):
+    result = width * height
+   
+    return result
+result = area(2,4)
+print(result)
+
+print(area(5,10))
+print(area(4,9))
+
+#questions 4 and 5
+
+def subtract(num1,num2):
+    result = num1 - num2
+    return result
+print(subtract(5,10))
+print(subtract(20,5))
+
+def divide(num1,num2):
+    result = num1 / num2
+    return result
+print(divide(12,3))
+print(divide(6,2))
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+print(add(5,6))
+print(add(9,12))
